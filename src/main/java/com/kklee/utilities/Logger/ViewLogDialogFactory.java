1f414de5ac8f4352851ce735df2749f4c7e7ab5f
kklee305 <kklee305@gmail.com>
2015-05-31 15:02:04 -0400

    changed dialog text size

--- src/main/java/com/kklee/utilities/Logger/ViewLogDialogFactory.java
+++ src/main/java/com/kklee/utilities/Logger/ViewLogDialogFactory.java
@@ -3,17 +3,21 @@ package com.kklee.utilities.Logger;
 import android.app.Activity;
 import android.app.Dialog;
 import android.support.v7.app.AlertDialog;
+import android.widget.TextView;
 
 /**
  * Created by Keith on 31/05/2015.
  */
 public class ViewLogDialogFactory {
 
-    public static Dialog createDialog(final Activity activity, String log) {
+    public static void showDialog(final Activity activity, String log) {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity, 2);
         builder.setTitle("Log")
                 .setMessage(log);
-        return builder.create();
+        Dialog dialog = builder.create();
+        dialog.show();
+        TextView tv = (TextView) dialog.findViewById(android.R.id.message);
+        tv.setTextSize(8);
     }
 
 }
