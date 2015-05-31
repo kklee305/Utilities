3fb48f06d787c16372491b4bbe46121ef7a56551
kklee305 <kklee305@gmail.com>
2015-05-31 15:09:59 -0400

    remove the string argument requirement

--- src/main/java/com/kklee/utilities/Logger/ViewLogDialogFactory.java
+++ src/main/java/com/kklee/utilities/Logger/ViewLogDialogFactory.java
@@ -2,6 +2,7 @@ package com.kklee.utilities.Logger;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.widget.TextView;
 
@@ -10,10 +11,10 @@ import android.widget.TextView;
  */
 public class ViewLogDialogFactory {
 
-    public static void showDialog(final Activity activity, String log) {
+    public static void showDialog(final Activity activity) {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity, 2);
         builder.setTitle("Log")
-                .setMessage(log);
+                .setMessage(activity.getSharedPreferences(Logger.LOGGER_SHARED_PREF, Context.MODE_PRIVATE).getString(Logger.LOGGER_SHARED_PREF, ""));
         Dialog dialog = builder.create();
         dialog.show();
         TextView tv = (TextView) dialog.findViewById(android.R.id.message);
